3f13ced8a9969adc2a5475ad5fecc173d0aabd93
Diego Freire <diegocfreire@gmail.com>
2020-04-09 15:42:43 -0300

    rotas e arquivo do postman para testes

--- src/app.js
+++ src/app.js
@@ -1,33 +1,79 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+/**
+ * GET: Lista todos os repositórios cadastrados.
+ */
+app.get("/repositories", (req, res) => {
+  return res.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+/**
+ * POST: Cadastra um novo repositório
+ */
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const r = {
+    "id": uuid(),
+    "title": title,
+    "url": url,
+    "techs": techs,
+    "likes": 0
+  }
+
+  repositories.push(r);
+
+  return res.json({message: 'Repositório adicionado com sucesso.'});
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+/**
+ * PUT: Altera as informações de um determinado repositório
+ */
+app.put("/repositories/:id", (req, res) => {
+  const { title, url, techs } = req.body;
+  const id = req.params.id;
+
+  const rIndex = repositories.findIndex(r => r.id == id);
+
+  repositories[rIndex].title = title;
+  repositories[rIndex].url = url;
+  repositories[rIndex].techs = techs;
+
+  return res.json({message: 'Repositório alterado com sucesso.'});
 });
 
+/**
+ * DELETE: Remove um repositório pelo id informado
+ */
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  repositories = repositories.filter(function(r) {
+    return r.id !== req.params.id
+  });
+
+  return res.json({message: 'Repositório removido com sucesso.'});
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+/**
+ * POST: Adiciona novo like ao repositório informado
+ */
+app.post("/repositories/:id/like", (req, res) => {
+  const id = req.params.id;
+
+  const rIndex = repositories.findIndex(r => r.id == id);
+
+  repositories[rIndex].likes++;
+
+  return res.json({message: 'Like adicionado com sucesso.'});
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 const app = require("./app");
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log(`🚀 Desafio 02 OnLine!`)
+});
